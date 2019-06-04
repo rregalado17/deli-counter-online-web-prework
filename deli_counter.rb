eae29705cc7ba42efccae8d043b505e91cd9f342
magical-flow-3186 <rregalado17@gmail.com>
2019-06-04 20:48:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,13 @@
 katza deli = []
 
+def line(array)
+  if array.count == 0 
+    "The line is currently empty"
+  else
+    response = "The line is currectly:"
+    array.each_with_index do |name, index|
+      response += "#{index + 1}. #{name}"
+    end
+    response
+  end
+end
