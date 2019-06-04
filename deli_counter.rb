02252581b154eccb0746ded18ccd5b3c0521128c
magical-flow-3186 <rregalado17@gmail.com>
2019-06-04 20:52:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,9 @@ katz_deli = []
 
 def line(array)
   if array.count == 0 
-    "The line is currently empty"
+    puts "The line is currently empty"
   else
-    response = "The line is currectly:"
-    array.each_with_index do |name, index|
+    response = "The line is currectly:" array.each_with_index do |name, index|
       response += "#{index + 1}. #{name}"
     end
     response
