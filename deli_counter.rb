4720942fe7f58fc23eb506b45780d53be459afb5
magical-flow-3186 <rregalado17@gmail.com>
2019-06-04 21:00:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(array, name)
   array.push(name)
     puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+  if array.count == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
