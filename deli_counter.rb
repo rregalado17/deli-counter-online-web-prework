4d5f9708161bc05ea2045efd43680c37059d10c3
magical-flow-3186 <rregalado17@gmail.com>
2019-06-04 20:54:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.count == 0 
     puts "The line is currently empty."
   else
-    response = "The line is currectly:" 
+    response = "The line is currently:" 
     array.each_with_index do |name, index|
       response += " #{index + 1}. #{name}"
     end
